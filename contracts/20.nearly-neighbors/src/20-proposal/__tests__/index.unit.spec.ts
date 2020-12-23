58ff0e8ba3742c563b8f4be5074e3d169497eac5
Sherif Abushadi <sherif@amgando.org>
2020-12-23 18:30:54 +0300

    mvp proposal contract with partial unit tests

---
+++ contracts/20.nearly-neighbors/src/20-proposal/__tests__/index.unit.spec.ts
@@ -0,0 +1,244 @@
+import { VM, VMContext, u128 } from "near-sdk-as";
+import * as contract from '../assembly'
+
+const ONE_NEAR = u128.from("1000000000000000000000000")
+
+const FACTORY_ACCOUNT_ID = 'neighbors.factory'
+
+const useFactoryAsPredecessor = (): void => {
+  VMContext.setPredecessor_account_id(FACTORY_ACCOUNT_ID)
+}
+
+const attachDeposit = (deposit: number): void => {
+  VMContext.setAttached_deposit(u128.mul(ONE_NEAR, u128.from(deposit)))
+}
+
+const attachMinDeposit = (): void => {
+  attachDeposit(3)
+}
+
+describe('20.nearly-neighbors.proposal', () => {
+  describe('initialize(): void', () => {
+    beforeEach(useFactoryAsPredecessor)
+
+    it('creates a new proposal, storing the factory account ID (predecessor)', () => {
+      attachMinDeposit()
+      contract.initialize()
+      expect(contract.get_factory()).toBe(FACTORY_ACCOUNT_ID)
+    })
+
+    it('requires a minimum deposit be attached', () => {
+      expect(() => {
+        contract.initialize()
+      }).toThrow()
+    })
+
+    it('is idempotent; will throw if already initialized', () => {
+      VMContext.setAttached_deposit(u128.mul(ONE_NEAR, u128.from(3)))
+      contract.initialize()
+
+      expect(() => {
+        contract.initialize()
+      }).toThrow()
+    })
+  });
+
+  describe('configure(title, description, goal, min_deposit): void', () => {
+    beforeEach(useFactoryAsPredecessor)
+    beforeEach(attachMinDeposit)
+
+    it('adds details and funding data to proposal', () => {
+      contract.initialize()
+    })
+
+    it('requires that the contract be initialized', () => {
+
+    })
+  });
+})
+
+// describe("00.orientation", () => {
+
+//   describe('Local interface', () => {
+//     describe('xcc()', () => {
+//       it('does not blow up on high level calls using function calls', () => {
+//         expect(() => {
+//           contract.xcc('high_fc', 'remote', 'do_some_work')
+//         }).not.toThrow()
+//       })
+//       it('does not blow up on high level calls using batch actions', () => {
+//         expect(() => {
+//           contract.xcc('high_ba', 'remote', 'do_some_work')
+//         }).not.toThrow()
+//       })
+//       it('does not blow up on low level calls using function calls', () => {
+//         expect(() => {
+//           contract.xcc('low_fc', 'remote', 'do_some_work')
+//         }).not.toThrow()
+//       })
+//       it('does not blow up on low level calls using batch actions', () => {
+//         expect(() => {
+//           contract.xcc('low_ba', 'remote', 'do_some_work')
+//         }).not.toThrow()
+//       })
+
+//     })
+//   })
+
+// })
+
+
+// ------------------------------------------------
+// ------------------------------------------------
+
+// import { u128, VM, VMContext, Context, env, base58, logging } from "near-sdk-as";
+// import * as contract from '../assembly'
+
+// const valid_account = 'alice'
+// const invalid_account = "XYZ"
+
+// let pk = "qSq3LoufLvTCTNGC3LJePMDGrok8dHMQ5A1YD9psbiz"
+// let deposit = u128.from("10000000000000000000000000"); // 10 NEAR
+
+// describe('LinkDrop', () => {
+//   beforeEach(() => {
+//     VMContext.setCurrent_account_id('linkdrop')
+//   })
+
+//   afterEach(() => {
+//     // cleanup storage between tests
+//   })
+
+//   describe('create_account', () => {
+//     it('allows the creation of a valid account', () => {
+//       contract.create_account(valid_account, pk)
+//       expect(() => {
+//         VMContext.setAttached_deposit(deposit)
+//         VMContext.setPrepaid_gas(deposit.toU64())
+//         contract.create_account(valid_account, pk)
+//       }).not.toThrow()
+
+//     })
+
+//     it('does not allow the creation of an invalid account', () => {
+//       expect(() => {
+//         VMContext.setAttached_deposit(deposit)
+//         contract.create_account(invalid_account, pk)
+//       }).toThrow() // Invalid account id
+//     })
+
+//     // TODO: verify that promise was created with funds for given username.
+//     //       but this needs simulation testing actually
+//   })
+
+//   describe('get_key_balance', () => {
+//     it('panics if an attempt is made to retrieve a missing account key', () => {
+//       expect(() => {
+//         contract.get_key_balance(pk);
+//       }).toThrow() // Key 'a::qSq3L...' is not present in the storage
+//     })
+
+//     it('is able to get missing balance', () => {
+//       // attach twice as much as needed
+//       const large_deposit = u128.mul(contract.ACCESS_KEY_ALLOWANCE, u128.from(2))
+//       VMContext.setAttached_deposit(large_deposit)
+//       contract.send(pk)
+
+//       // try getting the balance of the key
+//       let balance: u128 = contract.get_key_balance(pk);
+//       expect(balance).toBe(u128.sub(large_deposit, contract.ACCESS_KEY_ALLOWANCE))
+//     })
+//   })
+
+
+//   describe('create_account_and_claim', () => {
+//     it('panics if an attempt is made to claim an invalid account', () => {
+//       const large_deposit = u128.mul(contract.ACCESS_KEY_ALLOWANCE, u128.from(2))
+//       VMContext.setAttached_deposit(large_deposit)
+//       contract.send(pk)
+//       // Now, send new transaction to link drop contract.
+//       VMContext.setPredecessor_account_id('linkdrop')
+//       // @willem: looks like this method is broken?  not sure
+//       // VMContext.setSigner_account_pk(pk) // TypeError: Reflect.get called on non-object
+//       // log(Context.senderPublicKey) // "HuxUynD5GdrcZ5MauxJuu74sGHgS6wLfCqqhQkLWK" from default context object
+
+//       expect(() => {
+//         let pk2 = "2S87aQ1PM9o6eBcEXnTR5yBAVRTiNmvj8J8ngZ6FzSca"
+//         contract.create_account_and_claim(invalid_account, pk2);
+//       }).toThrow() // Invalid account id
+//     })
+
+//     // @willem: can't test this bc of the TypeError thrown on setSigner_account_pk()
+//     // which means i can't actually claim the key as another signer
+//     xit('works for a valid drop claim', () => {
+//       expect(env.isValidAccountID("alice")).toBeTruthy()
+//       // Deposit money to linkdrop contract.
+//       const large_deposit = u128.mul(contract.ACCESS_KEY_ALLOWANCE, u128.from(100))
+//       VMContext.setSigner_account_id("alice")
+//       VMContext.setAttached_deposit(large_deposit)
+//       contract.send(pk)
+
+//       // Now, send new transaction to link drop contract.
+//       VMContext.setPredecessor_account_id('linkdrop')
+//       // @willem: looks like this method is broken?  not sure
+//       // let public_key_arr = contract.decodePk(pk)
+//       // let canonical_pk = base58.encode(public_key_arr)
+//       // VMContext.setSigner_account_pk(canonical_pk) // TypeError: Reflect.get called on non-object
+//       // log(Context.senderPublicKey) // "HuxUynD5GdrcZ5MauxJuu74sGHgS6wLfCqqhQkLWK" from default context object
+//       VMContext.setAccount_balance(large_deposit)
+
+//       let pk2 = "2S87aQ1PM9o6eBcEXnTR5yBAVRTiNmvj8J8ngZ6FzSca"
+//       contract.create_account_and_claim(valid_account, pk2);
+//       // expect(() => {
+//       // }).not.toThrow()
+//       // @willem: this ends up throwing here bc the default key doesn't match the expected signer key
+//       // @willem: i guess we can fudge it now and expect the default key but would be better to fix so
+//       //          we're not introducing a magical key somewhere
+//       // Key 'a::HuxUynD5GdrcZ5MauxJuu74sGHgS6wLfCqqhQkLWK' is not present in the storage
+
+//       // TODO: verify that proper promises were created.
+//       //       but this needs simulation testing actually
+//     })
+//   })
+
+//   describe('send two times', () => {
+//     xit('increases linkdrop balance', () => {
+//       // Deposit money to linkdrop contract.
+//       const large_deposit = u128.mul(contract.ACCESS_KEY_ALLOWANCE, u128.from(2))
+//       VMContext.setCurrent_account_id('linkdrop')
+//       VMContext.setAttached_deposit(large_deposit)
+//       contract.send(pk)
+//       // assert_eq!(contract.get_key_balance(pk.clone()), (deposit - ACCESS_KEY_ALLOWANCE).into());
+//       expect(contract.get_key_balance(pk) == u128.sub(deposit, contract.ACCESS_KEY_ALLOWANCE))
+
+//       // Deposit money to linkdrop contract.
+//       VMContext.setCurrent_account_id('linkdrop')
+//       VMContext.setAttached_deposit(u128.add(u128.from(1), large_deposit))
+//       contract.send(pk)
+
+//       // assert_eq!(contract.accounts.get(& pk.into()).unwrap(), deposit + deposit + 1 - 2 * ACCESS_KEY_ALLOWANCE);
+//       expect(contract.get_key_balance(pk) == u128.sub(deposit, contract.ACCESS_KEY_ALLOWANCE))
+
+//       // @willem: my goal was to calculate this
+//       // deposit + deposit + 1 - 2 * ACCESS_KEY_ALLOWANCE
+//       // const expected_value = u128.add(deposit, u128.add(deposit, u128.sub(u128.from(1), u128.mul(u128.from(2), contract.ACCESS_KEY_ALLOWANCE))))
+//       // @willem: but then this happened
+//       /*
+//       WARNING AS217: Function '~lib/as-bignum/integer/u128/u128.add' cannot be inlined into itself.
+
+//        const expected_value = u128.add(deposit, u128.add(deposit, u128.sub(u128.from(1), u128.mul(u128.from(2), contract.ACCESS_KEY_ALLOWANCE))));
+//       */
+//       // @willem: so i did this.  good, bad or ugly?
+//       // deposit + deposit + 1 - 2 * ACCESS_KEY_ALLOWANCE
+//       // deposit + deposit + 1 - c
+//       // deposit + deposit + b
+//       // deposit + a
+//       const c = u128.mul(u128.from(2), contract.ACCESS_KEY_ALLOWANCE)
+//       const b = u128.sub(u128.from(1), c)
+//       const a = u128.add(deposit, b)
+//       const expected_value = u128.add(deposit, a)
+
+//       expect(contract.accounts.get(pk) == expected_value)
+//     })
+//   })
+// })
--- contracts/20.nearly-neighbors/src/20-proposal/assembly/index.ts
+++ contracts/20.nearly-neighbors/src/20-proposal/assembly/index.ts
@@ -1,71 +1,114 @@
+// @nearfile out
 import { u128, context, storage, PersistentVector, PersistentSet, ContractPromise } from "near-sdk-as"
 
 type AccountId = string
 
+const ONE_NEAR = u128.from("1000000000000000000000000")
+const XCC_GAS = 5000000000000 // 5 teragas
+const MIN_ACCOUNT_BALANCE = u128.mul(ONE_NEAR, u128.from(3)) // 3 NEAR min to keep account alive via storage staking
+
 @nearBindgen
 class Proposal {
+  constructor(
+    public factory: AccountId,
+    public funding: ProposalFunding | null = null,
+    public details: ProposalDetails | null = null,
+  ) { }
+}
+
+@nearBindgen
+class ProposalFunding {
+  constructor(
+    public goal: u128,
+    public min_deposit: u128 = MIN_ACCOUNT_BALANCE,
+    public total: u128 = u128.Zero,
+    public funded: bool = false,
+  ) { }
+}
+
+@nearBindgen
+class ProposalDetails {
   constructor(
     public title: string,
     public description: string,
     public author: AccountId
-  ) {}
+  ) { }
 }
 
 @nearBindgen
 class Supporter {
   constructor(
-    public account: AccountId, 
-    public amount: u128, 
+    public account: AccountId,
+    public amount: u128,
     // public coordinates: GeoCoords
-  ) {}
+  ) { }
 }
 
 @nearBindgen
 class GeoCoords {
   constructor(
-    public latitude: string, 
+    public latitude: string,
     public longitude: string
-  ) {}
+  ) { }
 }
 
 const supporters = new PersistentVector<Supporter>('s')
 
-const KEY_FACTORY = "fa"
-const KEY_METADATA = "m"
-const KEY_FUNDED = "f"
-const KEY_TOTAL_FUNDING = "tf"
-const KEY_MIN_DEPOSIT = "md"
-const KEY_FUNDING_GOAL = "fg"
-const ONE_NEAR = u128.from("1000000000000000000000000")
-const XCC_GAS = 5000000000000 // 5 teragas
-const MIN_ACCOUNT_BALANCE = u128.mul(ONE_NEAR, u128.from(3)) // 3 NEAR min to keep account alive via storage staking
+const PROPOSAL_KEY = "state"
 
-// TODO: assert attached minimum account balance
+// ----------------------------------------------------------------------------
+// CONTRACT methods
+// ----------------------------------------------------------------------------
+
 export function initialize(): void {
-  storage.set(KEY_FACTORY, context.predecessor)
-  storage.set(KEY_METADATA, false)
-  storage.set(KEY_TOTAL_FUNDING, u128.Zero)
-  storage.set(KEY_FUNDED, false)
+  assert(!is_initialized(), "Contract is already initialized.")
+  assert(u128.ge(context.attachedDeposit, MIN_ACCOUNT_BALANCE), "MIN_ACCOUNT_BALANCE must be attache to initialize (3 NEAR)")
+  // setup basic proposal structure
+  const proposal = new Proposal(context.predecessor)
+
+  resave_proposal(proposal)
 }
 
+export function configure(title: string, description: string, goal: u128, min_deposit: u128): void {
+  assert(is_initialized(), "Contract must be initialized first.")
+  const proposal = get_proposal()
+  proposal.details = new ProposalDetails(title, description, context.sender)
+  proposal.funding = new ProposalFunding(goal, min_deposit)
+
+  resave_proposal(proposal)
+}
+
+export function get_proposal(): Proposal {
+  assert(is_initialized(), "Contract must be initialized first.")
+  return storage.getSome<Proposal>(PROPOSAL_KEY)
+}
+
+/**
+ * Block UX from proposal details page until fully configured
+ */
 export function is_configured(): bool {
-  return storage.getSome<bool>(KEY_METADATA)
-}
-
-export function configure(title: string, description: string, funding_goal: u128, min_deposit: u128): void {
-  const proposal = new Proposal(title, description, context.sender)
-  storage.set(KEY_METADATA, proposal)
-  
-  storage.set(KEY_FUNDING_GOAL, funding_goal)
-  storage.set(KEY_MIN_DEPOSIT, min_deposit)  
+  assert(is_initialized(), "Contract must be initialized first.")
+  return !!get_proposal().details
 }
 
 export function get_factory(): AccountId {
-  return storage.getSome<AccountId>(KEY_FACTORY)
+  assert(is_initialized(), "Contract must be initialized first.")
+
+  return get_proposal().factory
 }
 
-export function get_funding(): u128 {
-  return storage.getSome<u128>(KEY_TOTAL_FUNDING)
+export function get_funding_total(): u128 {
+  assert(is_configured(), "Contract must be configured first.")
+
+  const proposal = storage.get<Proposal>(PROPOSAL_KEY)!
+  return proposal.funding!.total
+}
+
+export function is_fully_funded(): bool {
+  assert(is_configured(), "Contract must be configured first.")
+  const funding = get_funding_total()
+  const goal = get_proposal().funding!.goal
+  return u128.ge(funding, goal)
 }
 
 // '<project title>.<proposal type>.neighborly.testnet'
@@ -73,55 +116,65 @@ export function get_funding(): u128 {
 //   my-cafe.business.proposal.neighborly.testnet
 //   my-cafe.business.project.neighborly.testnet << created once funding has been met
 export function toString(): string {
-  const proposal = storage.getSome<Proposal>("m")
-  // const funding_goal = storage.get(KEY_FUNDING_GOAL)!
-  // const min_deposit = storage.get(KEY_MIN_DEPOSIT)!
-  
-  return "title: ["+ proposal.title + "]"
+  assert(is_configured(), "Contract must be configured first.")
+
+  const proposal = get_proposal()
+
+  return "title: [" + proposal.details!.title + "]"
 }
 
 // export function add_supporter(coordinates: GeoCoords): void {
 export function add_supporter(): void {
+  assert(is_configured(), "Contract must be configured first.")
+
   // TODO: add assertion for is_configured
-  assert(!storage.getSome<bool>(KEY_FUNDED), "Already funded")
-  
+  assert(!is_fully_funded(), "Proposal is already fully funded.")
+
   const amount = context.attachedDeposit
-  const min_deposit = storage.get<u128>(KEY_MIN_DEPOSIT)!
-  assert(u128.ge(context.attachedDeposit, min_deposit), "Please attach minimum deposit of [" + toNEAR(min_deposit) + "] NEAR")
+  const proposal = get_proposal()
+  assert(u128.ge(context.attachedDeposit, proposal.funding!.min_deposit), "Please attach minimum deposit of [" + toNEAR(proposal.funding!.min_deposit) + "] NEAR")
 
   const account = context.sender
-  
+
   // const supporter = new Supporter(account, amount, coordinates)
   const supporter = new Supporter(account, amount)
   supporters.push(supporter)
-  
+
   add_funding(amount)
 }
 
 export function list_supporters(): PersistentVector<Supporter> {
+  assert(is_configured(), "Contract must be configured first.")
+
   return supporters
 }
 
-function add_funding(amount: u128): void {
-  const current_total = storage.getSome<u128>(KEY_TOTAL_FUNDING)
-  const new_amount = u128.add(amount, current_total)
-  storage.set(KEY_TOTAL_FUNDING, new_amount)
-  track_funded()
+// ----------------------------------------------------------------------------
+// INTERNAL functions
+// ----------------------------------------------------------------------------
+
+function is_initialized(): bool {
+  return !!storage.hasKey(PROPOSAL_KEY)
 }
 
-function track_funded(): void  {
-  const total_funding = storage.get<u128>(KEY_TOTAL_FUNDING)!
-  const funding_goal = storage.get<u128>(KEY_FUNDING_GOAL)!
-  
-  const funded = u128.ge(total_funding, funding_goal)
-  // const funded = total_funding > funding_goal
-  
-  storage.set(KEY_FUNDED, funded)
-  
-  if(funded) {
+
+function add_funding(amount: u128): void {
+  const current_total = get_funding_total()
+  const new_amount = u128.add(amount, current_total)
+
+  const proposal = get_proposal()
+  const funding = proposal.funding!
+
+  funding.total = u128.sub(new_amount, MIN_ACCOUNT_BALANCE)
+  funding.funded = u128.ge(funding.total, funding.goal)
+  proposal.funding = funding // is this line necessary or is the reference maintained?  i think it's maintained and this is not needed
+
+  resave_proposal(proposal)
+
+  if (funding.funded) {
     create_project()
   }
-} 
+}
 
 function toNEAR(amount: u128): string {
   return u128.div(amount, ONE_NEAR).toString()
@@ -129,22 +182,24 @@ function toNEAR(amount: u128): string {
 
 // once funded, create a project
 // if this is done by the factory, then easier to version
-export function create_project(): void {
-  const factory = storage.getSome<AccountId>(KEY_FACTORY)
-  const proposal = storage.getSome<Proposal>(KEY_METADATA)
+function create_project(): void {
+  const proposal = get_proposal()
   const projectBudget = u128.sub(context.accountBalance, MIN_ACCOUNT_BALANCE)
+
   ContractPromise.create(
-    factory,                  // target contract account name
+    proposal.factory,         // target contract account name
     "create_project",         // target method name
-    proposal,                 // target method arguments
+    proposal.details,         // target method arguments
     XCC_GAS,                  // gas attached to the call (~5 Tgas (5e12) per "hop")
     projectBudget             // deposit attached to the call
   )
 }
 
-
+function resave_proposal(proposal: Proposal): void {
+  storage.set(PROPOSAL_KEY, proposal)
+}
 
 // other functions:
 //  cancelProposal = if not funded by due date, then abort
 //    reimburseFunds = return funds to supporters
-//  transferFunds = move funding to project, assign to 
+//  transferFunds = move funding to project, assign to
