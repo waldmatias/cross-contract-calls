fd5fbc1e14b739931171d10f0c003b99c4469817
Tanner Welsh <tannerwelsh@gmail.com>
2021-01-27 21:00:24 -0700

    Add note about revisiting MIN_ACCOUNT_BALANCE

--- contracts/20.nearly-neighbors/src/utils.ts
+++ contracts/20.nearly-neighbors/src/utils.ts
@@ -6,6 +6,9 @@ import { u128 } from 'near-sdk-as';
  * ONE_NEAR = unit of NEAR token in yocto NEAR (1e24)
  * XCC_GAS = gas for cross-contract calls, ~5 Tgas (teragas = 1e12) per "hop"
  * MIN_ACCOUNT_BALANCE = 3 NEAR min to keep account alive via storage staking
+ *
+ * TODO: revist MIN_ACCOUNT_BALANCE after some real data is included b/c this
+ *  could end up being much higher
  */
 export const ONE_NEAR = u128.from('1000000000000000000000000');
 export const XCC_GAS = 5000000000000;
